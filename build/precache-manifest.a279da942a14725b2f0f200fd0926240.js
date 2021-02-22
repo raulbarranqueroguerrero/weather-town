7936641aa21d6474bd1bd060a5d28d2522f589ad
raulbarranqueroguerrero <barranqueroguerrero@gmail.com>
2021-02-22 22:10:12 +0100

    Delete build from gitignore

---
+++ build/precache-manifest.a279da942a14725b2f0f200fd0926240.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "7a6c35df7699e14e7735",
+    "url": "/static/css/main.38f70b01.chunk.css"
+  },
+  {
+    "revision": "7a6c35df7699e14e7735",
+    "url": "/static/js/main.681fca41.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "05961bf9731a9a09de2e",
+    "url": "/static/css/2.add651c6.chunk.css"
+  },
+  {
+    "revision": "05961bf9731a9a09de2e",
+    "url": "/static/js/2.742c0e00.chunk.js"
+  },
+  {
+    "revision": "9915fef980fa539085da55b84dfde760",
+    "url": "/static/media/remixicon.9915fef9.woff2"
+  },
+  {
+    "revision": "881fbc46361e0c0e5f003c159b2f3005",
+    "url": "/static/media/remixicon.881fbc46.woff"
+  },
+  {
+    "revision": "31d28485e1cf7369272270fd730327c0",
+    "url": "/static/media/remixicon.31d28485.eot"
+  },
+  {
+    "revision": "888e61f04316f10bddfff7bee10c6dd0",
+    "url": "/static/media/remixicon.888e61f0.ttf"
+  },
+  {
+    "revision": "95138f36e015ad912c37db92164f5844",
+    "url": "/static/media/remixicon.95138f36.svg"
+  },
+  {
+    "revision": "1288ea8ea1da4327bc1a9ab70b98e04d",
+    "url": "/index.html"
+  }
+];
